10d093223a5c78dde083cf682eeb78a87811acd0
Vikesh <vpawar@ind1.echo.quinstreet.net>
2018-02-06 18:40:04 +0530

    commit now

--- aloknath.js
+++ aloknath.js
@@ -2,12 +2,49 @@
 //setTimeout(function(){ alert(":SDf");document.getElementById('u_0_2').click(); }, 3000);
 //document.getElementById("Mobility").selectedIndex = 12;
 
+//Login into AgentPortal Stage
+/*document.getElementById('un').value = 'vpawar_cc';
+document.getElementById('pw').value = 'H^Fw0&j8';
+document.getElementsByTagName('button')[0].click();
+
+
+setTimeout(function(){
+	alert("versionv2")
+	window.location.search += "&version=v2"
+},5000)*/
+
+// TestdataDebug panel for Education
+document.getElementById('test_data_selector').selectedIndex=4;
+document.getElementsByName('IgnoreTimeRangeEvaluation')[0].selectedIndex=1;
+document.getElementsByName('SourceDataCaptureKey')[0].value='0';
+document.getElementsByName('PhoneProgramKey')[0].selectedIndex=24;
+document.getElementsByClassName('green')[0].click();
+PhoneProgramKey
 //Filling up the AgentPortal form for edu
 
-document.getElementsByName("FN")[0].value = 'Vikesh' ;
+/*document.getElementsByName("FN")[0].value = 'Vikesh' ;
 document.getElementsByName("LN")[0].value = 'Pawar' ;
 document.getElementsByName("InterestedInEducation")[0].selectedIndex=1;
+
+//Goals PANEL
 //$('#cc-panel-Service #ProgramsOfInterestType > u').click()
 //document.getElementById("ui-id-218").click();
 document.getElementsByName("DegreeStartTimeframe")[0].selectedIndex=1;
 document.getElementsByName("EducationLevel")[0].selectedIndex=8;
+document.getElementsByName("HighSchoolGradYear")[0].selectedIndex=5;
+document.getElementsByName("DegreeOfInterest")[5].click();//bachelor Degree
+document.getElementsByName("DegreeOfInterest")[6].click();//Master's Degree
+
+//About PANEL
+document.getElementsByName("Gender")[0].selectedIndex=1;
+document.getElementsByName("Age")[0].selectedIndex=9;
+document.getElementsByName("S1")[0].value='12the Lane, Test Street';
+document.getElementsByName("PC")[0].value='94404';
+document.getElementsByName("HP")[0].value='6505787758';
+document.getElementsByName("EM")[0].value='vikesh@test.com';
+document.getElementsByName("ComputerAccess")[0].selectedIndex=1;
+window.scrollTo(0, 1500);
+
+document.getElementsByClassName("process green")[0].click();
+
+*/
